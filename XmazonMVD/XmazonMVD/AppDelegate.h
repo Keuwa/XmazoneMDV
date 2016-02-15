0fbd2fa3ad4164d6c9e183c52c9fefe77e587976
Keuwa <vincent.durpoix@gmail.com>
2016-02-15 17:01:02 +0100

    Bon j’ai créé le projet ! J’affiche un label !!! 20/20 assuré

---
+++ XmazonMVD/XmazonMVD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XmazonMVD
+//
+//  Created by Pinson Celine on 15/02/2016.
+//  Copyright (c) 2016 Pinson Celine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XmazonMVD/XmazonMVD/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  XmazonMVD
+//
+//  Created by Pinson Celine on 15/02/2016.
+//  Copyright (c) 2016 Pinson Celine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
