dd067527598066381e4569b4fb9dd48f99b26e71
Wayne Kyle <waynekyle@waynes-mac.local>
2016-03-01 18:44:57 +0100

    Ajout des layouts pour l'tutilisateur

---
+++ XmazonMVD/XmazonMVD/BaseMainViewController.h
@@ -0,0 +1,25 @@
+//
+//  BaseMainViewController.h
+//  XmazonMVD
+//
+//  Created by Wayne Kyle on 01/03/2016.
+//  Copyright © 2016 Pinson Celine. All rights reserved.
+//
+
+
+/*
+ 
+ This view is dedicated to allow the user to choose between:
+ 
+ - admin
+ 
+ or
+ 
+ - user
+ 
+ */
+#import <UIKit/UIKit.h>
+
+@interface BaseMainViewController : UIViewController
+
+@end
---
+++ XmazonMVD/XmazonMVD/SubscribeViewController.h
@@ -0,0 +1,13 @@
+//
+//  SubscribeView.h
+//  XmazonMVD
+//
+//  Created by Wayne Kyle on 01/03/2016.
+//  Copyright © 2016 Pinson Celine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubscribeViewController : UIViewController
+
+@end
